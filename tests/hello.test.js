731e272fdb504c0de981d8e6582ff30820e25ec3
Fanjie Wang <frank@anomalyinnovations.com>
2017-06-02 01:08:42 -0400

    V1

---
+++ tests/hello.test.js
@@ -0,0 +1,17 @@
+import * as hello from '../controllers/hello';
+
+test('hello', async () => {
+  const event = {};
+  const context = {};
+  const expected = {
+    statusCode: 200,
+    body: JSON.stringify({version: 'v1'}),
+  };
+
+  function callback(error, result) {
+    expect(result).toEqual(expected);
+  }
+
+  expect.assertions(1);
+  await hello.main(event, context, callback);
+});
