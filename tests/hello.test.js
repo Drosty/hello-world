920e7e8802ee85ed13c304e1e493f470c50d6b59
Fanjie Wang <frank@anomalyinnovations.com>
2017-06-10 12:44:52 -0500

    v15-10

--- tests/hello.test.js
+++ tests/hello.test.js
@@ -5,7 +5,7 @@ test('hello', async () => {
   const context = {};
   const expected = {
     statusCode: 200,
-    body: JSON.stringify({version: 'v15-9'}),
+    body: JSON.stringify({version: 'v15-10'}),
   };
 
   function callback(error, result) {
