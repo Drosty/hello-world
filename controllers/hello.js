285d4ada539e48a3d4403dd5973413849a578904
Fanjie Wang <frank@anomalyinnovations.com>
2017-06-22 17:53:31 -0400

    v19-6

--- controllers/hello.js
+++ controllers/hello.js
@@ -2,7 +2,7 @@ export const main = async (event, context, callback) => {
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      version: 'v19-5',
+      version: 'v19-6',
       message: process.env.MESSAGE
     })
   });
