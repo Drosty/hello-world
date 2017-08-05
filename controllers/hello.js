5e7efb4efdd864d6a591fbe0c5084649a18eb88b
Frank <bb@utmuser1-4.wireless.utoronto.ca>
2017-08-05 18:11:23 -0400

    v31

--- controllers/hello.js
+++ controllers/hello.js
@@ -2,7 +2,7 @@ export const main = async (event, context, callback) => {
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      version: 'v30',
+      version: 'v31',
       message: process.env.MESSAGE
     })
   });
